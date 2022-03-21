b89bc0b4ab2ac09589f49dca41a5a09df5220edb
OctopusCosmic <zhan3447@purdue.edu>
2022-03-21 16:22:14 -0400

    capture by webcam

--- Real-time detection.py
+++ Real-time detection.py
@@ -2,6 +2,8 @@
 
 import cv2
 import time
+
+
 def main():
     # Input
     source = 0
@@ -10,18 +12,23 @@ def main():
     # Start capture video by laptop
     camera = cv2.VideoCapture(source) # 0 for laptop camera, 1 for external camera
     # Get frame captured by camera
+    t0 = time.time()
     ret, frame = camera.read()
+    i = 1
     while ret:
         # Show frames
         cv2.imshow("preview", frame)
         # Get frame captured by camera
         ret, frame = camera.read()
+        i += 1
+        t1 = time.time()
+        if t1-t0 > 1:
+            print(i)
 
         # Key to trigger stop capturing
         key = cv2.waitKey(20)
         if key == 27:  # exit on ESC
             break
-
         # use buffer to hold several frames
         # How many frames per seconds
 
